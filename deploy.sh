140c410d0402cd5bb77e80c751432e0e36b5674c
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-14 11:14:32 -0800

    Document deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,37 @@
 #!/bin/zsh
 
+# Setup
 git switch main
 npm i
 HASH=$(git log -n 1 --pretty=format:"%h")
 DATE=$(date '+%B %d, %Y')
+
+# Build
 echo "{\"commitHash\": \"$HASH\", \"buildDate\": \"$DATE\"}" > src/lib/staticdata.json
 npm run build
 git restore src/lib/staticdata.json
+
+# Switch to new branch
 git switch --orphan deploy/$HASH
+
+# Expand build into current directory
+mv build/* ./
+rm -r build
+
+# Add nojekyll and gitignore
+touch .nojekyll
 echo "/node_modules" >> .gitignore
 echo "/.svelte-kit" >> .gitignore
 echo ".DS_Store" >> .gitignore
-mv build/* ./
-rm -r build
+
+# Commit deployment
 git rm -r --cached .
 git add .
 git commit -m "Add deployment for $HASH"
+
+# Merge into deploy/current
 git switch deploy/current
-git merge -X theirs deploy/$HASH --allow-unrelated-histories -m "Add deployment for $HASH"
+git merge -X theirs deploy/$HASH --allow-unrelated-histories --squash -m "Add deployment for $HASH"
+
 git branch -d deploy/$HASH
 git switch main

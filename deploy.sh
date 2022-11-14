df85a6f09ebe8a384e6da200c5271cb513fcbca7
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-14 10:36:26 -0800

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+git switch main
+HASH=$(git log -n 1 --pretty=format:"%h")
+DATE=$(date '+%B %d, %Y')
+echo "{\"commitHash\": \"$HASH\", \"buildDate\": \"$DATE\"}" > src/lib/staticdata.json
+npm run build
+git restore src/lib/staticdata.json
+git switch --orphan deploy/$HASH
+echo "/node_modules" > .gitignore
+echo "/.svelte-kit" > .gitignore
+echo ".DS_Store" > .gitignore
+mv build/* ./
+rm -r build
+git restore src/lib/staticdata.json
+git add .
+git commit -m "Add deployment for $HASH"
+git switch deploy/current
+git merge -X theirs deploy/$HASH --allow-unrelated-histories
+git branch -d deploy/$HASH
+git switch main

438bebf09287ad0dd1f13b412ccadc7fa285267a
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-15 09:51:30 -0800

    Refactor footer sliding code to be internal

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,16 +3,35 @@
     import { slide } from "svelte/transition";
 
     export let sliding = false;
+
+    let shown = false;
+    $: visible = (!sliding) || shown;
+    const repoLink = "https://github.com/thegail/thegail.github.io";
 </script>
 
-<footer transition:slide={{ duration: sliding ? 400 : 0 }}>
-    <span>
-        &copy; thegail 2022 /
-        &#x1F3F7; <code>{staticData.commitHash}</code> /
-        &#x1F528; {staticData.buildDate} /
-        &#x1F9F1; with <a href="https://kit.svelte.dev" target="_blank">SvelteKit</a>
-    </span>
-</footer>
+{#if visible}
+    <footer 
+        transition:slide={{ duration: sliding ? 400 : 0 }}
+        on:mouseleave={() => shown = false}
+        on:mouseenter={() => shown = true}
+    >
+        <span>
+            &copy; thegail 2022 /
+            &#x1F3F7; <a href={repoLink} target="_blank"><code>{staticData.commitHash}</code></a> /
+            &#x1F528; {staticData.buildDate} /
+            &#x1F9F1; with <a href="https://kit.svelte.dev" target="_blank">SvelteKit</a>
+        </span>
+    </footer>
+    <div
+        class="hoverdetector"
+        on:mouseenter={() => shown = true}
+        on:mouseleave={() => shown = false}
+    >
+        &nbsp;
+    </div>
+{:else}
+    <span class="dot" on:mouseenter={() => shown = true} />
+{/if}
 
 <style>
     footer {
@@ -20,5 +39,25 @@
         display: flex;
         justify-content: center;
         border-top: 2px solid var(--secondary);
+    }
+
+    .dot {
+        border-radius: 50%;
+        background: var(--secondary);
+        border: none;
+        height: 10px;
+        width: 10px;
+        padding: 0;
+        position: absolute;
+        right: 5px;
+        bottom: 5px;
+    }
+
+    .hoverdetector {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        width: 40px;
+        z-index: 0;
     }
 </style>

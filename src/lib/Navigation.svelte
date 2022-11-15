5f73b2d6b723e9bbf9424e5196b5809d63b3e086
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-14 20:53:16 -0800

    Refactor navigation into a component

---
+++ src/lib/Navigation.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { page } from "$app/stores";
+
+    function tagValue(path) {
+        return $page.url.pathname === path ? "span" : "a";
+    }
+</script>
+
+<nav>
+    <svelte:element this={tagValue("/")} href="/">home</svelte:element> /
+    <svelte:element this={tagValue("/about")} href="/about">about</svelte:element> /
+    <svelte:element this={tagValue("/code")} href="/code">code</svelte:element> /
+    <svelte:element this={tagValue("/contacts")} href="/contacts">contacts</svelte:element>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        gap: 5px;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        margin: 0;
+        border-top: 2px solid var(--secondary);
+        border-bottom: 2px solid var(--secondary);
+    }
+</style>
--- src/routes/(root)/+layout.svelte
+++ src/routes/(root)/+layout.svelte
@@ -1,22 +1,12 @@
 <script>
-    import { onMount } from "svelte";
-    import { page } from "$app/stores";
     import staticData from "$lib/staticdata.json";
 	import Banner from "$lib/Banner.svelte";
-
-    function tagValue(path) {
-        return $page.url.pathname === path ? "span" : "a";
-    }
+	import Navigation from "$lib/Navigation.svelte";
 </script>
 
 <header>
     <Banner />
-    <nav>
-        <svelte:element this={tagValue("/")} href="/">home</svelte:element> /
-        <svelte:element this={tagValue("/about")} href="/about">about</svelte:element> /
-        <svelte:element this={tagValue("/code")} href="/code">code</svelte:element> /
-        <svelte:element this={tagValue("/contacts")} href="/contacts">contacts</svelte:element>
-    </nav>
+    <Navigation />
 </header>
 <main>
     <div class="innermain">
@@ -37,18 +27,6 @@
         display: flex;
         flex-direction: column;
         align-items: center;
-    }
-
-    nav {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        gap: 5px;
-        padding-top: 5px;
-        padding-bottom: 5px;
-        margin: 0;
-        border-top: 2px solid var(--secondary);
-        border-bottom: 2px solid var(--secondary);
     }
 
     main {

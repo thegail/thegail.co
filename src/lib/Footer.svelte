d8150bd37aa7ca270252622e680fc8c3ee8adaa4
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-14 23:24:09 -0800

    Extract footer into a component

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+    import staticData from "$lib/staticdata.json";
+</script>
+
+<footer>
+    <span>
+        &copy; thegail 2022 /
+        &#x1F3F7; <code>{staticData.commitHash}</code> /
+        &#x1F528; {staticData.buildDate} /
+        &#x1F9F1; with <a href="https://kit.svelte.dev" target="_blank">SvelteKit</a>
+    </span>
+</footer>
+
+<style>
+    footer {
+        display: flex;
+        justify-content: center;
+        border-top: 2px solid var(--secondary);
+    }
+</style>
--- src/routes/(root)/+layout.svelte
+++ src/routes/(root)/+layout.svelte
@@ -1,26 +1,21 @@
 <script>
-    import staticData from "$lib/staticdata.json";
 	import Banner from "$lib/Banner.svelte";
 	import Navigation from "$lib/Navigation.svelte";
+	import Footer from "$lib/Footer.svelte";
 </script>
 
 <header>
     <Banner />
     <Navigation />
 </header>
+
 <main>
     <div class="innermain">
         <slot />
     </div>
 </main>
-<footer>
-    <span>
-        &copy; thegail 2022 /
-        &#x1F3F7; <code>{staticData.commitHash}</code> /
-        &#x1F528; {staticData.buildDate} /
-        &#x1F9F1; with <a href="https://kit.svelte.dev" target="_blank">SvelteKit</a>
-    </span>
-</footer>
+
+<Footer />
 
 <style>
     header {
@@ -37,11 +32,5 @@
     .innermain {
         padding-left: 20%;
         padding-right: 20%;
-    }
-
-    footer {
-        display: flex;
-        justify-content: center;
-        border-top: 2px solid var(--secondary);
     }
 </style>

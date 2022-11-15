78f872d70f60bd8b1ac7956570d4462ff9a0adef
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-14 19:03:18 -0800

    Refactor some layout to root

--- src/routes/(root)/+layout.svelte
+++ src/routes/(root)/+layout.svelte
@@ -2,13 +2,10 @@
     import { onMount } from "svelte";
     import { page } from "$app/stores";
     import staticData from "$lib/staticdata.json";
-    import faviconLight from "$lib/assets/favicon.svg";
-    import faviconDark from "$lib/assets/favicon_dark.svg";
     import bannerLight from "$lib/assets/fullsize_transparent.png";
     import bannerDark from "$lib/assets/fullsize_transparent_dark.png";
 
     let isDarkMode = false;
-    $: favicon = isDarkMode ? faviconDark : faviconLight;
     onMount(() => {
         isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
         console.log(isDarkMode);
@@ -19,17 +16,8 @@
     }
 </script>
 
-<svelte:head>
-    <title>thegail</title>
-    <link rel="apple-touch-icon" href={favicon} />
-    <link rel="icon" type="image/png" href={favicon} />
-    <link rel="mask-icon" href={favicon} color="#202020" />
-    <link rel="shortcut icon" href={favicon} />
-    <meta name="theme-color" content={isDarkMode ? "#202020" : "#D0D0D0"} />
-</svelte:head>
-
 <header>
-    <!-- <img class="banner" src={isDarkMode ? bannerDark : bannerLight} alt="thegail" /> -->
+    <img class="banner" src={isDarkMode ? bannerDark : bannerLight} alt="thegail" />
     <nav>
         <svelte:element this={tagValue("/")} href="/">home</svelte:element> /
         <svelte:element this={tagValue("/about")} href="/about">about</svelte:element> /
@@ -91,50 +79,5 @@
         display: flex;
         justify-content: center;
         border-top: 2px solid var(--secondary);
-    }
-
-    :global(*) {
-        color: var(--secondary);
-        font-family: Lato, Helvetica, sans-serif;
-    }
-
-    :global(body) {
-        background: var(--primary);
-        padding-left: 20px;
-        padding-right: 20px;
-        display: flex;
-        flex-direction: column;
-        width: calc(100% - 40px);
-        height: 100%;
-        margin: 0;
-        overflow: hidden;
-    }
-
-    :global(html) {
-        width: 100%;
-        height: 100%;
-    }
-
-    :global(ul) {
-        list-style-type: ">   ";
-    }
-
-    :global(code) {
-        font-family: Menlo, monospace;
-        font-size: 0.9em;
-    }
-
-    @media(prefers-color-scheme: light) {
-        :global(:root) {
-            --primary: #D0D0D0;
-            --secondary: #202020;
-        }
-    }
-
-    @media(prefers-color-scheme: dark) {
-        :global(:root) {
-            --primary: #202020;
-            --secondary: #D0D0D0;
-        }
     }
 </style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,71 @@
+<script>
+    import faviconLight from "$lib/assets/favicon.svg";
+    import faviconDark from "$lib/assets/favicon_dark.svg";
+	import { onMount } from "svelte";
+
+    let isDarkMode = false;
+    onMount(() => {
+        isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        console.log(isDarkMode);
+    });
+
+    $: favicon = isDarkMode ? faviconDark : faviconLight;
+</script>
+
+<svelte:head>
+    <title>thegail</title>
+    <link rel="apple-touch-icon" href={favicon} />
+    <link rel="icon" type="image/png" href={favicon} />
+    <link rel="mask-icon" href={favicon} color="#202020" />
+    <link rel="shortcut icon" href={favicon} />
+    <meta name="theme-color" content={isDarkMode ? "#202020" : "#D0D0D0"} />
+</svelte:head>
+
+<slot />
+
+<style>
+    :global(*) {
+        color: var(--secondary);
+        font-family: Lato, Helvetica, sans-serif;
+    }
+
+    :global(body) {
+        background: var(--primary);
+        padding-left: 20px;
+        padding-right: 20px;
+        display: flex;
+        flex-direction: column;
+        width: calc(100% - 40px);
+        height: 100%;
+        margin: 0;
+        overflow: hidden;
+    }
+
+    :global(html) {
+        width: 100%;
+        height: 100%;
+    }
+
+    :global(ul) {
+        list-style-type: ">   ";
+    }
+
+    :global(code) {
+        font-family: Menlo, monospace;
+        font-size: 0.9em;
+    }
+
+    @media(prefers-color-scheme: light) {
+        :global(:root) {
+            --primary: #D0D0D0;
+            --secondary: #202020;
+        }
+    }
+
+    @media(prefers-color-scheme: dark) {
+        :global(:root) {
+            --primary: #202020;
+            --secondary: #D0D0D0;
+        }
+    }
+</style>

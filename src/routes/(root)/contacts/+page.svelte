970ba2e6db3aa1018787f3b137eb969998dee5ad
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-17 08:29:03 -0800

    Add basic contacts page

--- src/routes/(root)/contacts/+page.svelte
+++ src/routes/(root)/contacts/+page.svelte
@@ -1,5 +1,15 @@
 <h1>Contacts</h1>
 
 <ul>
-    <li>Hi!</li>
+	<li>discord: <strong>thegail#7362</strong></li>
+	<li>
+		<a href="https://github.com/thegail" target="_blank">
+			github: <strong>@thegail</strong>
+		</a>
+	</li>
+	<li>
+		<a href="mailto:losses.foxfire_00@icloud.com">
+			email (proxy address): <strong>losses.foxfire_00@icloud.com</strong>
+		</a>
+	</li>
 </ul>

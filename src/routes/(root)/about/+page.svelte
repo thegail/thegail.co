67cc6e1fde56ee5057c39fb8ef86c9829b46111a
Teddy Gaillard <teddy.gaillard@icloud.com>
2023-07-02 17:08:32 -0700

    Fix that gosh darn about page

--- src/routes/(root)/about/+page.svelte
+++ src/routes/(root)/about/+page.svelte
@@ -4,11 +4,11 @@
 
 <p>
 	I make things sometimes. Occasionally those things are interesting enough to show to people, so I
-	put them here. I&rsquo;m a chronic project abandoner, so expect many half-baked ideas.
+	put them here. I&rsquo;m a chronic project abandoner so expect a lot of halfway thought-through ideas.
 </p>
 
 <p>
-	Other things about me: I run sometimes (hoping to go under five this season). I like making music
+	Other things about me: I run sometimes (4:44 1600). I like making music
 	(guitar mostly). <span class="nobreak">日本語を少し話せる。</span> If you couldn&rsquo;t already tell
 	from this site&rsquo;s design, I&rsquo;m an extremely boring person, so I don&rsquo;t have much to
 	say. Thanks for stopping by though.
